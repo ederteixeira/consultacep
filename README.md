becd35613835c05b34576903cb439566bceaa8ad
ederteixeira <ccbeder@gmail.com>
2021-01-24 03:07:43 -0300

    Inicio projeto

---
+++ README.md
@@ -0,0 +1,5 @@
+## Consulta CEP 
+
+Consulta de API em python
+
+
---
+++ consultacep.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+cep = input("Informe o seu CEP: ")
+
+try:
+    req = requests.get('https://cep.awesomeapi.com.br/json/'+cep)
+    formatado = json.loads(req.text)
+    print(formatado['address'])
+    print(formatado['district'])
+    print(formatado['city'], "/", formatado['state'])
+except:
+    print("Erro", formatado['status']," - ", formatado['message'])
+
